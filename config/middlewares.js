9217c8d15fab0e246571311c7cb9a9085e7c0dd9
Indrajit <indrajitghosh.dev@gmail.com>
2024-12-27 19:08:24 +0530

    middlewire update

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,11 @@ module.exports = [
           ],
           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "script-src": ["https://cdn.ckeditor.com"],
+          "script-src": [
+            "'self'", // Allow scripts from the same domain
+            "https://cdn.ckeditor.com", // Existing CKEditor script source
+            "https://nest-api-04r6.onrender.com", // Add the blocked script source
+          ],
           upgradeInsecureRequests: null,
         },
       },
@@ -28,3 +32,34 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
+
+// module.exports = [
+//   "strapi::logger",
+//   "strapi::errors",
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": [
+//             "'self'",
+//             "https:",
+//             "https://proxy-event.ckeditor.com",
+//           ],
+//           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+//           "script-src": ["https://cdn.ckeditor.com"],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
